9b79fdbd95e21b3b16f9b18360400f405b272b0a
Sun Lifeng <1104506375@qq.com>
2016-02-18 11:18:24 +0800

    TypeScript 初探

---
+++ code-practice/js/public.ts
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @ufree (1104506375@qq.com)
+ * @date    2016-02-18 10:34:47
+ * @version $Id$
+ */
+
+// Hello World
+var hw:string="Hello World!";  // 定义字符串变量
+document.write("<h1>"+hw+"</h1>"); 
